192bcc1f48bd031bdafd90f20b8c0d98b1442fb5
Arya Elfren <aryaelfren@aryaelfren.eu>
2024-11-02 22:22:58 +0000

    benchmark main

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,39 @@
 const std = @import("std");
+const mem = std.mem;
+const TM = @import("TM.zig");
 
-pub fn main() !void {}
+pub fn main() error{BadParam}!u8 {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const ally = arena.allocator();
 
-test "init TM" {
-    const TM = @import("TM.zig");
-    const empty: TM = .empty;
-    _ = empty;
+    var args = try std.process.argsWithAllocator(ally);
+    defer args.deinit();
+
+    std.debug.assert(args.skip());
+
+    const eval_choice = args.next() orelse return error.BadParam;
+    const tm_choice = args.next() orelse return error.BadParam;
+
+    const tm: TM = if (mem.eql(u8, tm_choice, "bb1"))
+        .bb1
+    else if (mem.eql(u8, tm_choice, "bb2"))
+        .bb2
+    else if (mem.eql(u8, tm_choice, "bb2"))
+        .bb3
+    else if (mem.eql(u8, tm_choice, "bb2"))
+        .bb4
+    else
+        return error.BadParam;
+
+    if (mem.eql(u8, eval_choice, "TM")) {
+        const eval: TM = .from(tm);
+        defer eval.deinit(ally);
+        
+        try eval.eval(ally);
+        const out = try eval.tape.to_array(ally);
+        defer ally.free(out);
+        
+        std.log.err("{any}", out);
+    } else return error.BadParam;
 }
