5c1259a0ced24b89e454d9e6082d0195e09b1a9f
Arya Elfren <aryaelfren@aryaelfren.eu>
2024-11-02 15:16:34 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.05";
+    zig2nix.inputs.nixpkgs.follows = "nixpkgs";
+    zig2nix.url = "github:Cloudef/zig2nix";
+  };
+
+  outputs = { self, ... }@inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } rec {
+    systems = inputs.nixpkgs.lib.systems.flakeExposed;
+    perSystem = { pkgs, config, system, ... }: {
+      devShells = {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            inputs.zig2nix.outputs.packages.${system}.zig.master.bin
+          ];
+        };
+      };
+    };
+  };
+}
